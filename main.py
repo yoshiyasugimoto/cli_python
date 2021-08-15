c33a4a1677462565be7b17e8cb4566683130d1ee
yoshiyasugimoto <gabesaiti@gmail.com>
2021-08-15 23:37:38 +0900

    create easy cli using python

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+
+def ggg():
+    x = sys.argv[1:]
+    print("値",x)
+    print("型",type(x))
+
+if __name__ == "__main__":
+    ggg()
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+        install_requires=[],#'sys' 
+        entry_points={
+            "console_scripts":[
+                "myapp = main:ggg"
+            ]
+        }
+)
